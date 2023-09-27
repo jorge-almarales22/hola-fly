cea6666db6f613014d611f5bb72fcf66a650425a
Jorge Almarales <jorgealmarales22@gmail.com>
2023-09-27 10:02:03 -0500

    Fix (getPeople): Agregando validaciones para validar que el ID es un numero valido.

--- src/controllers/hfswapi.controller.js
+++ src/controllers/hfswapi.controller.js
@@ -3,37 +3,53 @@ import { getHomeWorldID } from "../helpers/getHomeWorldID.js";
 
 export const getPeople = async(req, res) => {
 
-    const { id } = req.params;
+    try {
+        
+        const { id } = req.params;
 
-    const { people } = database;
-
-    const person = people.find(person => person.id == id);
-
-    if (!person) {
-
-        const response = await fetch(`https://swapi.dev/api/people/${id}`);
-        const data = await response.json();
-
-        const { name, mass, height, homeworld } = data;
-
-        const newPerson = {
-            name,
-            mass,
-            height
+        if(isNaN(Number(id))){
+            return res.status(400).json({
+                message: "ID must be a number"
+            })
+        }
+    
+        const { people } = database;
+    
+        const person = people.find(person => person.id == id);
+    
+        if (!person) {
+    
+            const response = await fetch(`https://swapi.dev/api/people/${id}`);
+            const data = await response.json();
+    
+            const { name, mass, height, homeworld } = data;
+    
+            const newPerson = {
+                name,
+                mass,
+                height
+            }
+    
+            const responseWorld = await fetch(data.homeworld);
+            const dataWorld = await responseWorld.json();
+    
+            const { name: homeworldName } = dataWorld;
+    
+            newPerson.homeworldName = homeworldName;
+    
+            newPerson.homeworldId = getHomeWorldID(homeworld);
+    
+            return res.status(200).json(newPerson);
+    
         }
 
-        const responseWorld = await fetch(data.homeworld);
-        const dataWorld = await responseWorld.json();
+        return res.status(200).json(person);
+        
+    } catch (error) {
 
-        const { name: homeworldName } = dataWorld;
-
-        newPerson.homeworldName = homeworldName;
-
-        newPerson.homeworldId = getHomeWorldID(homeworld);
-
-        return res.status(200).json(newPerson);
-
+        console.log(error)
+        
     }
+    
 
-    return res.status(200).json(person);
 }
