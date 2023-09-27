3019ace205cf7c32e82ccbae387426ea89e0e2d0
Jorge Almarales <jorgealmarales22@gmail.com>
2023-09-27 16:19:25 -0500

    Refactor (middleware): Eliminando la carpeta middleware

--- src/routes/hfswapi.route.js
+++ src/routes/hfswapi.route.js
@@ -1,10 +1,9 @@
 import { Router } from "express";
 import { getLogs, getPeople, getPlanet, getWeightOnPlanetRandom } from "../controllers/hfswapi.controller.js";
-import { logsMiddleware } from "../middlewares/logs.middleware.js";
 
 export const hfswapiRouter = Router();
 
-hfswapiRouter.get("/getPeople/:id", logsMiddleware, getPeople);
-hfswapiRouter.get("/getPlanet/:id", logsMiddleware, getPlanet);
-hfswapiRouter.post("/getWeightOnPlanetRandom", logsMiddleware, getWeightOnPlanetRandom);
+hfswapiRouter.get("/getPeople/:id", getPeople);
+hfswapiRouter.get("/getPlanet/:id", getPlanet);
+hfswapiRouter.post("/getWeightOnPlanetRandom", getWeightOnPlanetRandom);
 hfswapiRouter.get("/getLogs", getLogs);
